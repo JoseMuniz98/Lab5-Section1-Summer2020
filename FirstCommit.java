a85765b3fb92bf3be85db84575f5f36a160664c5
JoseMuniz98 <joseluismuniz98@gmail.com>
2020-07-13 15:03:24 -0500

    This is my first commit

---
+++ FirstCommit.java
@@ -0,0 +1,6 @@
+public class FirstCommit{
+	public static void(String[] args){
+		System.out.println("My name is Jose Muniz");
+		System.out.println("One of my hobbies is playing the piano");
+	}
+}
